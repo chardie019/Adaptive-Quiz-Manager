71d0d104d34ec311a439b7095789b9db417cc069
chart08 <connor.hart08@gmail.com>
2015-05-25 05:45:52 +1000

    First version of record answer script

--- record-answer.php
+++ record-answer.php
@@ -1,14 +1,52 @@
 <?php
 
-/* 
-@connor, if you want to do the recording of the results, 
- * that would be sweet! 
- * If i was you, id make another php file called record answer or sonething 
- * and include it just before take quiz includes takequiz view (before next page displays).
-
-You should be able to see the required information/variables on the take quiz page.
- */
-
 // include php files here 
 require_once("includes/config.php");
 // end of php file inclusion
+
+//declares needed variable
+global $dbLogic;
+
+$dbLogic = new DB();
+
+//Creates a new result if one doesn't exist
+if ($_SESSION["RESULT_ID" == ""]) {
+    
+    //Gets result start time.
+    $startDate = date('Y-m-d H:i:s');
+    
+    //Array to be inserted into DB
+    $data1 = array(
+        "user_USERNAME" => $_SESSION["username"],
+        "quiz_QUIZ_ID" => $_SESSION["QUIZ_CURRENT_QUIZ_ID"],
+        "STARTED_AT" => $startDate,
+    );
+    
+    //Inserts data into result table, stores the new result's ID
+    $_SESSION["RESULT_ID"] = $dbLogic->insert($data1, result);
+
+}
+
+//Gets answer datetime.
+$answerDate = date('Y-m-d H:i:s');
+
+//Checks if question has been answered before this session,
+//if so gets how many times.
+$data2 = array(
+       "result_RESULT_ID" => $_SESSION["RESULT_ID"],
+       "question_QUESTION_ID" => $_SESSION["QUIZ_CURRENT_QUESTION"],
+        );
+
+$answeredData = $dbLogic->select("*", "result_answer", $data2);
+
+$passNo = count($answeredData) + 1;
+
+$data3 = array(
+       "result_RESULT_ID" => $_SESSION["RESULT_ID"],
+       "question_QUESTION_ID" => $_SESSION["QUIZ_CURRENT_QUESTION"],
+       "PASS_NO" => $passNo,
+       "ANSWER" => $answerPosted,
+       "ANSWERED_AT" => $answerDate,
+       );
+
+$dbLogic->insert($data3, result_answer);
