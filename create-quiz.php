bfb049da2636c1a623c66a38ef570a7f73b9c8a1
AQMharry <hbailey92@hotmail.com>
2015-07-29 12:26:08 +1000

    Create Quiz
    Implemented Ccreate Quiz use case:
    - accepts and validates user input.
    - DOESNT add user to editor table
    - DOESNT check if quiz name already exists in DB
    - NEED to update 'quiz-list' page to display image now that quiz cover
    images can be uploaded

--- create-quiz.php
+++ create-quiz.php
@@ -10,5 +10,257 @@
 require_once("includes/config.php");
 // end of php file inclusion
 
+//Set page error messages blank upon initial loading
+$quizNameError = " ";
+$invalidDateError1 = " ";
+$invalidDateError2 = " ";
+$dayStartError = " ";
+$monthStartError = " ";
+$yearStartError = " ";
+$dayEndError = " ";
+$monthEndError = " ";
+$yearEndError = " ";
+$imageUploadError = " ";
+
+//If form is submitted, run this section of code, otherwise just load the view
+//Get values from submitted form
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    
+    $quizName = filter_input(INPUT_POST, "quizName");
+    $quizDescription = filter_input(INPUT_POST, "quizDescription");
+    $isPublic = filter_input(INPUT_POST, "isPublic");
+    $noAttempts = filter_input(INPUT_POST, "noAttempts");
+    $isTime = filter_input(INPUT_POST, "isTime");
+    $timeHours = filter_input(INPUT_POST, "timeHours");
+    $timeMinutes = filter_input(INPUT_POST, "timeMinutes");
+    $isSave = filter_input(INPUT_POST, "isSave");
+    
+    //Validate opening dates entered by user
+    $monthStart = filter_input(INPUT_POST, "monthStart");
+    $dayStart = filter_input(INPUT_POST, "dayStart");
+    $yearStart = filter_input(INPUT_POST, "yearStart");
+    
+    //Validate closing dates entered by user
+   $monthEnd = filter_input(INPUT_POST, "monthEnd");
+   $dayEnd = filter_input(INPUT_POST, "dayEnd");
+   $yearEnd = filter_input(INPUT_POST, "yearEnd");
+   
+   //Get image values entered by user
+   $quizImageText = filter_input(INPUT_POST, "quizImageText");
+    
+   if($quizName == " " || $quizName ==""){
+        $quizNameError = "Error: You must enter a name for your quiz.";
+        include("create-quiz-view.php");
+        exit();//Loads create-quiz-view into create-quiz.php, keeps same URL
+   }
+   
+   
+    //Ensure only months with 31 days are selected for Opening dates
+    //Ensure February dates are are within the months limits
+    if (($monthStart=="January" || $monthStart=="March" || $monthStart=="May" || 
+            $monthStart=="July"  || $monthStart=="August" || $monthStart=="October" || 
+            $monthStart=="December" && $dayStart=="31") || $monthStart=="February" && $dayStart=="29" || $dayStart=="30" || $dayStart=="31"){
+        //Error on days selected dont match month limits
+        //Load error screen up. 
+        $invalidDateError1 = "Error: Quiz Open month doesn't have that many days.";
+        include("create-quiz-view.php");
+        exit();//Loads create-quiz-view into create-quiz.php, keeps same URL
+    }
+    
+    //Ensure only months with 31 days are selected for Closing dates
+    if (($monthEnd=="January" || $monthEnd=="March" || $monthEnd=="May" || 
+            $monthEnd=="July"  || $monthEnd=="August" || $monthEnd=="October" || 
+            $monthEnd=="December" && $dayEnd=="31") || $monthEnd=="February" && $dayEnd=="29" || $dayEnd=="30" || $dayEnd=="31"){
+        //Error on days selected dont match month limits
+        //Load error screen up. 
+        $invalidDateError2 = "Error: Quiz Close month doesn't have that many days.";
+        include("create-quiz-view.php");
+        exit();//Loads create-quiz-view into create-quiz.php, keeps same URL
+    }
+    
+    //Ensure date hasnt passed already and is set EQUAL to or AFTER current system date
+    //Get current system date values in the same format as user entererd them
+    
+    $currentDate = getdate(date("U"));
+    
+    //Check START date is set AFTER current system date
+    if($currentDate['year'] == $yearStart){
+        if($currentDate['mon'] == $monthStart){
+            if($currentDate['mday'] <= $dayStart){
+               //Do something, set a flag? Or just have it continue down program?              
+            }
+            else{               
+                //Load up same page, don't insert quiz into the database, stop processes
+                $dayStartError = "Error: Opening date has already passed.";
+                include("create-quiz-view.php");
+                exit();//Loads create-quiz-view into create-quiz.php, keeps same URL
+            }
+        }
+        else if($currentDate['mon'] < $monthStart){           
+              //Do something, set a flag? Or just have it continue down program?   
+            }
+        
+        else{
+            //Load up same page, don't insert quiz into the database, stop processes
+            $monthStartError = "Error: Opening date has already passed.";
+            include("create-quiz-view.php");
+            exit();//Loads create-quiz-view into create-quiz.php, keeps same URL
+        }
+    }
+    else if($currentDate['year'] < $yearStart){
+        //Do something, set a flag? Or just have it continue down program?   
+    }
+    else{
+
+        //Load up same page, don't insert quiz into the database, stop processes
+        $yearStartError = "Error: Opening date has already passsed.";
+        include("create-quiz-view.php");
+        exit();//Loads create-quiz-view into create-quiz.php, keeps same URL
+    }
+    
+    //Check whether CLOSE date is after START date (Which also means it's after current date). 
+    if($yearEnd == $yearStart){
+        if($monthEnd == $monthStart){
+            if($dayEnd >= $dayStart){
+                //Do something, set a flag? Or just have it continue down program?   
+            }
+            else{
+                //Load up same page, don't insert quiz into the database, stop processes
+                $dayEndError = "Error: Closing date must be after Opening date.";
+                include("create-quiz-view.php");
+                exit();//Loads create-quiz-view into create-quiz.php, keeps same URL
+            }
+        }
+        else if($monthEnd > $monthStart){  
+            //Do something, set a flag? Or just have it continue down program?   
+            }
+        
+        else{
+            //Load up same page, don't insert quiz into the database, stop processes
+            $monthEndError = "Error: Closing date must be after Opening date.";
+            include("create-quiz-view.php");
+            exit();//Loads create-quiz-view into create-quiz.php, keeps same URL
+        }
+    }
+    else if($yearEnd > $yearStart){
+            //Do something, set a flag? Or just have it continue down program?   
+    }
+    else{
+        //Load up same page, don't insert quiz into the database, stop processes
+        $yearEndError = "Error: Closing date must be after Opening date.";
+        include("create-quiz-view.php");
+        exit();//Loads create-quiz-view into create-quiz.php, keeps same URL
+    }   
+    
+
+    /*Validate Image upload
+     * 
+     *Swap out $target_dir value with your local path for testing, will change it on SCCI server when uploaded
+     *Double \\ is needed at the end of path to cancel out the single \ effect leading into "
+     * 
+     */
+    $target_dir = "C:\Users\Admin\Documents\GitHub\Adaptive-Quiz-Manager\data\quiz-images\\";
+    $target_file = $target_dir . basename($_FILES["quizImageUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    
+    // Check if image file is an actual image or fake image
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            $imageUploadError = "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            $imageUploadError = "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+    // Check if file already exists inside folders
+    if (file_exists($target_file)) {
+        $imageUploadError = "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+    // Check file size is smaller than 500kb, can change this later
+    if ($_FILES["quizImageUpload"]["size"] > 500000) {
+        $imageUploadError = "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    // Allow certain image file types only *Stop people uploading other file types e.g. pdf
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an upload error. Exit if true.
+    if ($uploadOk == 0) {
+        $imageUploadError = "Error: There was an error with your image upload. Please check the following: \n"
+                . "- File size is 500kb or less "
+                . "- File must be in .jpg, .png, .jpeg and .gif file types\n"
+                . "- The name of your file may be taken. Try renaming the file ";
+        
+        include("create-quiz-view.php");
+        exit();//Loads create-quiz-view into create-quiz.php, keeps same URL
+        
+    // If image passed all criteria, attempt to upload
+    } else {
+        if (move_uploaded_file($_FILES["quizImageUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["quizImageUpload"]["name"]). " has been uploaded.";
+        } else {
+            $imageUploadError = "Sorry, there was an error uploading your file.";
+            include("create-quiz-view.php");
+            exit();//Loads create-quiz-view into create-quiz.php, keeps same URL
+        }
+    }
+    
+    //Image alt validation needed? Insert here if required
+    
+    
+    $dbLogic = new DB();
+    //Get username for use with Editors table when required
+    $uid = $_SESSION["username"];
+    //Set time limit to 00:00:00 for storing in database if there is NO time limit
+    if($isTime == '0'){
+        $isTime = '00:00:00';
+    }
+    
+    //Set Number of attempts to 0 for storing in database if there are unlimited attempts
+    if($noAttempts == 'Unlimited'){
+        $noAttempts = '0';
+    }
+    
+    //Create String value for dateStart and dateEnd values
+    
+    $dateOpen = $yearStart."-".$monthStart."-".$dayStart." 00:00:00";
+    $dateClose = $yearEnd."-".$monthEnd."-".$dayEnd." 11:59:00";    
+    
+    $dataArray = array(
+        "QUIZ_ID" => "",
+        "QUIZ_NAME" => "$quizName",
+        "DESCRIPTION" => "$quizDescription",
+        "IS_PUBLIC" => "$isPublic",
+        "NO_OF_ATTEMPTS" => "$noAttempts",
+        "TIME_LIMIT" => "$isTime",
+        "IS_SAVABLE" => "$isSave",
+        "DATE_OPEN" => $dateOpen,
+        "DATE_CLOSED" => $dateClose,
+        "INTERNAL_DESCRIPTION" => "",
+        "IMAGE" => basename( $_FILES["quizImageUpload"]["name"]),
+        "IMAGE_ALT" => "$quizImageText"
+        
+        );
+
+    
+    ($dbLogic->insert($dataArray, "quiz"));
+     
+    include("create-quiz-view.php");
+    
+    /*Also need to:
+     * - Insert the username of creator into the editors table
+     * - Validate whether the Quiz name already exists in DB
+    */
+    
+    
+
+}
 //html
-include("create-quiz-view.php");
+else{
+    include("create-quiz-view.php");}
--- views/create-quiz-view.php
+++ views/create-quiz-view.php
@@ -24,18 +24,240 @@ require_once("includes/config.php");
             <?php include('nav.php'); ?>
 
  
-<div id="content">
+            <div id="content-create-quiz">
  
-<h1>Create Quiz</h1>
+                <h1>Create Quiz</h1>
+
+                <br />
+                <br />
+                <!--enctype="" used because of image file upload in form-->
+                <form action='#' method='post' enctype="multipart/form-data" >
+    
+                <label>Please enter the Title of your quiz: </label>
+                <input type='text' name='quizName' size='30'></input>
+                <br />
+                <?php echo "<span class=\"inputError\">".$quizNameError."</span>"?>
+                <br />
+                <br />
+
+                <label>Please enter a description for your quiz: </label>
+                <textarea name="quizDescription" cols="40" rows="5"></textarea>
+                <br />
+                <br />
+
+                <label>Is this public or private: </label>
+                Public: 
+                <input type='radio' name='isPublic' value='1' checked></input>
+                Private: 
+                <input type='radio' name='isPublic' value='0'></input>
+                <br />
+                <br />
+
+                <label>Please enter the number of attempts permitted: </label>
+                <select name='noAttempts'>
+                    <option>Unlimited</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                </select>
+                <br />
+                <br />
+
+                <label>Is there a time limit to complete the quiz: </label>
+                No:
+                <input type='radio' name='isTime' value='0' checked></input>
+                Yes:
+                <input type='radio' name='isTime' value='1'></input>
+                <br />
+                <br />
+                <label>***If YES, please enter that time limit: </label>
+                Hours
+                <select name='timeHours'>
+                    <option>0</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+                Minutes
+                <select name='timeMinutes'>
+                    <option>0</option>
+                    <option>5</option>
+                    <option>10</option>
+                    <option>15</option>
+                    <option>20</option>
+                    <option>25</option>
+                    <option>30</option>
+                    <option>35</option>
+                    <option>40</option>
+                    <option>45</option>
+                    <option>50</option>
+                    <option>55</option>
+                </select> 
+                <br />
+                <br />
+
+                <label>Can users save progress and return later to the quiz: </label>
+                No:
+                <input type='radio' name='isSave' value='0' checked></input>
+                Yes:
+                <input type='radio' name='isSave' value='1'></input>
+                <br />
+                <br />
+                <label for="dayStart">When does this quiz open:</label>
+                <select name="dayStart" /> 
+                    <option>1</option>       
+                    <option>2</option>       
+                    <option>3</option>       
+                    <option>4</option>       
+                    <option>5</option>       
+                    <option>6</option>       
+                    <option>7</option>       
+                    <option>8</option>       
+                    <option>9</option>       
+                    <option>10</option>       
+                    <option>11</option>       
+                    <option>12</option>       
+                    <option>13</option>       
+                    <option>14</option>       
+                    <option>15</option>       
+                    <option>16</option>       
+                    <option>17</option>       
+                    <option>18</option>       
+                    <option>19</option>       
+                    <option>20</option>       
+                    <option>21</option>       
+                    <option>22</option>       
+                    <option>23</option>       
+                    <option>24</option>       
+                    <option>25</option>       
+                    <option>26</option>       
+                    <option>27</option>       
+                    <option>28</option>       
+                    <option>29</option>       
+                    <option>30</option>       
+                    <option>31</option>       
+                </select> - 
+                <select name="monthStart" /> 
+                    <option>1</option>       
+                    <option>2</option>       
+                    <option>3</option>       
+                    <option>4</option>       
+                    <option>5</option>       
+                    <option>6</option>       
+                    <option>7</option>       
+                    <option>8</option>       
+                    <option>9</option>       
+                    <option>10</option>       
+                    <option>11</option>       
+                    <option>12</option>       
+                </select> - 
+                <select name="yearStart" />     
+                    <option>2015</option>       
+                    <option>2016</option>       
+                    <option>2017</option>       
+                    <option>2018</option>       
+                </select> 
+                <span class="dateOrder">(Day-Month-Year)</span> 
+                <br />
+                <?php echo "<span class=\"inputError\">".$dayStartError."</span>"?>
+                <?php echo "<span class=\"inputError\">".$monthStartError."</span>"?>
+                <?php echo "<span class=\"inputError\">".$yearStartError."</span>"?>
+                <?php echo "<span class=\"inputError\">".$invalidDateError1."</span>"?>
+                <br />
+                <br />
+                <label for="dayEnd">When does this quiz close: </label> 
+                <select name="dayEnd" /> 
+                    <option>1</option>       
+                    <option>2</option>       
+                    <option>3</option>       
+                    <option>4</option>       
+                    <option>5</option>       
+                    <option>6</option>       
+                    <option>7</option>       
+                    <option>8</option>       
+                    <option>9</option>       
+                    <option>10</option>       
+                    <option>11</option>       
+                    <option>12</option>       
+                    <option>13</option>       
+                    <option>14</option>       
+                    <option>15</option>       
+                    <option>16</option>       
+                    <option>17</option>       
+                    <option>18</option>       
+                    <option>19</option>       
+                    <option>20</option>       
+                    <option>21</option>       
+                    <option>22</option>       
+                    <option>23</option>       
+                    <option>24</option>       
+                    <option>25</option>       
+                    <option>26</option>       
+                    <option>27</option>       
+                    <option>28</option>       
+                    <option>29</option>       
+                    <option>30</option>       
+                    <option>31</option>       
+                </select> - 
+                <select name="monthEnd" /> 
+                    <option>1</option>       
+                    <option>2</option>       
+                    <option>3</option>       
+                    <option>4</option>       
+                    <option>5</option>       
+                    <option>6</option>       
+                    <option>7</option>       
+                    <option>8</option>       
+                    <option>9</option>       
+                    <option>10</option>       
+                    <option>11</option>       
+                    <option>12</option>       
+                </select> - 
+                <select name="yearEnd" />     
+                    <option>2015</option>       
+                    <option>2016</option>       
+                    <option>2017</option>       
+                    <option>2018</option>       
+                </select> 
+                <span class="dateOrder">(Day-Month-Year)</span> 
+                <br />
+                <?php echo "<span class=\"inputError\">".$dayEndError."</span>"?>
+                <?php echo "<span class=\"inputError\">".$monthEndError."</span>"?>
+                <?php echo "<span class=\"inputError\">".$yearEndError."</span>"?>
+                <?php echo "<span class=\"inputError\">".$invalidDateError2."</span>"?>
+                <br />
+                <br />
+                
+                <label>Is there a cover image to upload with your quiz:</label>
+                <input type="file" name="quizImageUpload" accept="image/*">
+                <br />
+                <?php echo "<span class=\"inputError\">".$imageUploadError."</span>"?>
+                <br />
+                <br />
+
+                <label>Please provide alternative text for the image you uploaded:</label>
+                <textarea name="quizImageText" cols="40" rows="5"></textarea>
+                <br />
+                <br />
+
+                <button class="mySubmit" type="submit" name="confirmQuiz" value="Enter">Create</button>
+            </form>
+
+            </div> <!-- end #content -->
 
 
+            <?php include('sidebar.php'); ?>
 
-</div> <!-- end #content -->
- 
-
-<?php include('sidebar.php'); ?>
- 
-<?php include('footer.php'); ?>
+            <?php include('footer.php'); ?>
  
         </div> <!-- End #wrapper -->
  
