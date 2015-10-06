0ef55084b0a5c58525799ae06652364db1e076f5
Joshua Graham <jgcomms@yahoo.com.au>
2015-10-07 02:19:48 +1100

    fixing merges

--- includes/views/edit-quiz-list-view.php
+++ includes/views/edit-quiz-list-view.php
@@ -1,21 +1,35 @@
 <?php
 
 $templateLogic = new templateLogic;
-$templateLogic->setTitle('Edit Quiz');
-$templateLogic->setHeading('Edit Quiz');
+$templateLogic->setTitle('Manage Quiz');
+$templateLogic->setHeading('Manage Quiz');
 $templateLogic->startBody();
 ?>
             
-   <div id="content-centre">
-                <h2>Update quiz information and content</h2>
-                
-                <span class="feedback-span"><?php echo $message; ?>
-                
+   <div id="content-centre">                  
+                <br />
+                <h3>Create Quiz</h3>
+                <br />
+                <p>
+                    Create a new adaptive quiz on a topic of your choosing. 
+                    <br />
+                    <br />
+                    
+                    <a href="<?php echo (CONFIG_ROOT_URL . "/create-quiz.php") ?>" class="mybuttonlink mySubmit">
+                            Create
+                    </a>
+                </p>
+                <br />
+                <br />
+                <br />
+                <br />
+
                 <?php if (count($quizEditId) > 0) { //there are quizes?>
 
                 <form action="#" method="post">
+                    <h3>Manage existing quiz</h3>
                     <div id='listWrapper'>
-                        <h4 class='lightLabel'>Select a quiz to attempt from the list below:</h4>
+                        <h4 class='lightLabel'>Select a quiz to manage from the list below:</h4>
                         <div id='listScroll'>
                             <div class="radios">
                             <?php foreach ($nameArray as $answerRow) { ?>
@@ -36,20 +50,19 @@ $templateLogic->startBody();
                     <br />
                     </p>
                     <p>
-                        Select a quiz from the list above to edit. It contains all quizzes on 
-                        which your account is listed with Edit permissions.
-                        Upon selection, you will be taken to the Edit Quiz Front Page, where 
-                        you can perform a number of actions including edit quiz, add/remove question, 
-                        add edit permissions for other users. 
+                        The quiz list contains all those quizzes for which you currently hold 'Edit' permissions.
+                        <br />
+                        <br />
+                        <button class="mybutton mySubmit" type="submit" name="selectQuiz" value="Select Quiz">
+                            Manage
+                        </button>
                     </p>
-                    
                     <br />
                     <br />
                     <?php echo "<input type=\"hidden\" name=\"selectQuizId\" value=". ($answerRow["QUIZ_ID"])." />"?>
 
-                        <button class="mybutton mySubmit" type="submit" name="selectQuiz" value="Select Quiz">
-                            Select Quiz
-                        </button>
+                        
+                        
                     </form>
                 <?php } else {  //there are NO quizes?>
                 <p>
@@ -57,7 +70,7 @@ $templateLogic->startBody();
                     <br />
                     <br />
                     How about having a go an creating a quiz?
-                    <a href="<?php echo (CONFIG_ROOT_URL . "/create-quiz") ?>" class="mybutton mySubmit">
+                    <a href="<?php echo (CONFIG_ROOT_URL . "/create-quiz.php") ?>" class="mybuttonlink mySubmit">
                             Create Quiz
                     </a>
                     <a href="<?php echo (CONFIG_ROOT_URL) ?>" class="mybutton myReturn">
